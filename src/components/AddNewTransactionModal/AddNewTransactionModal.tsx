e4792d410572c1b99a9c98c34678e91d9c98eaec
Andrei Cavalcante <andreisilva96832@gmail.com>
2026-02-14 16:18:31 -0300

    chore: rename interface and remove title and description props

--- src/components/AddNewTransactionModal/AddNewTransactionModal.tsx
+++ src/components/AddNewTransactionModal/AddNewTransactionModal.tsx
@@ -1,18 +1,16 @@
-interface GenericModalProps {
-    title: string;
-    description: string;
+interface AddNewTransactionProps {
     isOpen: boolean;
     setIsOpen: (isOpen: boolean) => void;
     action: () => void;
 }
 
-export const AddNewTransactionModal = ({ title, description, isOpen, setIsOpen, action }: GenericModalProps) => {
+export const AddNewTransactionModal = ({ isOpen, setIsOpen, action }: AddNewTransactionProps) => {
     return (
         <div className={`fixed inset-0 z-50 flex items-center justify-center ${isOpen ? 'block' : 'hidden'}`}>
             <div className="absolute inset-0 bg-black opacity-50" onClick={() => setIsOpen(false)}></div>
             <div className="relative rounded-lg bg-white p-6 shadow-lg">
-                <p className="mb-4 text-xl font-semibold">{title}</p>
-                <p className="mb-6 text-sm text-gray-600">{description}</p>
+                <p className="mb-4 text-xl font-semibold">Titulo</p>
+                <p className="mb-6 text-sm text-gray-600">descrição</p>
                 <div className="flex justify-end space-x-4">
                     <button
                         className="rounded-md bg-gray-300 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-400"
